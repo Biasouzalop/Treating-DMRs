f5c4bbf25a92e1e4116f128e3a73126b273b742a
Biasouzalop <82969162+Biasouzalop@users.noreply.github.com>
2021-11-11 09:31:20 -0300

    Tracks
    Use special file with coordinates in ensembl format

---
+++ DMRs_regfeatures.py
@@ -0,0 +1,35 @@
+from OpenClose import Arquivo
+coords = Arquivo('dmrs_JME_coordsforbiomart.csv').abrir(0)
+
+#Importando biomart server
+from biomart import BiomartServer
+server = BiomartServer("http://www.ensembl.org/biomart")
+
+# Definindo base de dados
+regulations = server.databases['ENSEMBL_MART_FUNCGEN']
+
+# Features = Reg. elements (CTCF Binding site, Promoter)
+reg_feature = regulations.datasets['hsapiens_regulatory_feature']
+
+final = []
+
+i = 0
+for x in coords[1:]:
+    response = reg_feature.search({
+        'filters':{
+            'chromosomal_region': x
+        }
+    },header = 0)
+
+    for line in response.iter_lines():
+        line = line.decode('utf-8')
+        
+        Arquivo('dmrs_JME_regfeatures.csv').salvar([x],[line.split("\t")],append_mode=True)
+    
+    print(i)
+    i += 1
+    
+
+
+
+
